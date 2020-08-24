266f1c17b6a9de94a269580cbf538534a236fdea
Chris Allbritton <cla.inbox@gmail.com>
2020-08-24 19:29:50 -0400

    Create objects in seed file

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,7 +2,6 @@ class Pet < ApplicationRecord
   belongs_to :shelter
 
   after_initialize :set_defaults
-  # has_one_attached :image
 
   validates_presence_of :name, :approximate_age, :sex
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+shelter_1 = Shelter.create!(name: "Willy's Animal Center",
+                             address: "2020 Whirlwind Dr",
+                             city: "Denver",
+                             state: "CO",
+                             zip: 80014)
+
+shelter_2 = Shelter.create!(name: "Wilmer's Place",
+                            address: "123 Fake St",
+                            city: "Chicago",
+                            state: "IL",
+                            zip: 60651)
+
+shelter_3 = Shelter.create!(name: "Luna's Animal Sanctuary",
+                            address: "3456 Lost Place",
+                            city: "Peoria",
+                            state: "IL",
+                            zip: 60564)
+
+pet_1 = shelter_1.pets.create!(image: "https://i.pinimg.com/236x/23/4d/b2/234db2abd5a83b872c345335fb9a4933--baby-rottweiler-cutest-puppy.jpg",
+                              name: "Waylon",
+                              description: "1 year old rottie pup",
+                              approximate_age: 1,
+                              sex: "male",
+                              adoption_status: "available")
+
+pet_2 = shelter_1.pets.create!(image: "https://usercontent1.hubstatic.com/2088428.jpg",
+                              name: "Merle",
+                              description: "Mostly a wolf",
+                              approximate_age: 5,
+                              sex: "male",
+                              adoption_status: "available")
+
+pet_3 = shelter_2.pets.create!(image: "https://wallpapersgood.com/resize/960x800/wallpapers/main2/201645/moana-moana-heihei-hej-hej.jpg ",
+                              name: "HeiHei",
+                              description: "1 year old pet rooster",
+                              approximate_age: 1,
+                              sex: "male",
+                              adoption_status: "available")
+
+pet_4 = shelter_2.pets.create!(image: "https://www.awesomestories.com/images/user/fc91b9a310fa92b98f27ab1038548062.jpg",
+                              name: "Wilbur",
+                              description: "Tiny baby pig",
+                              approximate_age: 1,
+                              sex: "male",
+                              adoption_status: "available")
+
+pet_5 = shelter_3.pets.create!(image: "https://i.pinimg.com/originals/52/4d/57/524d571a2e0d5bcb54f0b819ae9aeb63.jpg",
+                              name: "Sandy",
+                              description: "Spits and bites",
+                              approximate_age: 4,
+                              sex: "female",
+                              adoption_status: "available")
+
+pet_6 = shelter_3.pets.create!(image: "https://cinefex.com/backissues/covers/Cinefex_108.jpg",
+                              name: "Charlotte",
+                              description: "All the eyes and legs",
+                              approximate_age: 6,
+                              sex: "female",
+                              adoption_status: "available")
