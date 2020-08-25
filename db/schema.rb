37f7d2c53e5ed5294edc9d432b74b8ce2caf0df9
Arique <arique.aguilar@gmail.com>
2020-08-25 17:19:11 -0500

    Adds foreign key to reviews from shelter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_220052) do
+ActiveRecord::Schema.define(version: 2020_08_25_221453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 2020_08_25_220052) do
     t.string "rating"
     t.string "content"
     t.string "optional_picture"
+    t.bigint "shelter_id"
+    t.index ["shelter_id"], name: "index_reviews_on_shelter_id"
   end
 
   create_table "shelters", force: :cascade do |t|
@@ -64,4 +66,5 @@ ActiveRecord::Schema.define(version: 2020_08_25_220052) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "pets", "shelters"
+  add_foreign_key "reviews", "shelters"
 end
