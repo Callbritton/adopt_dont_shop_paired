31867ca8ef2908a791f5c14b287a4e71b48e4eb6
Chris Allbritton <cla.inbox@gmail.com>
2020-08-27 10:19:27 -0400

    Update review update action and review edit test to pass

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,10 +6,10 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    shelter = Shelter.find(params[:shelter_id])
-    review = shelter.reviews.new(review_params)
+    @shelter = Shelter.find(params[:shelter_id])
+    review = @shelter.reviews.new(review_params)
       if review.save
-        redirect_to "/shelters/#{params[:shelter_id]}"
+        redirect_to "/shelters/#{@shelter.id}"
       else
         flash[:notice] = "Form Entry Error: fill in a title, rating, and content in order to submit a shelter review"
         render :new
@@ -18,6 +18,16 @@ class ReviewsController < ApplicationController
 
   def edit
     @review = Review.find(params[:review_id])
+  end
+
+  def update
+    review = Review.find(params[:review_id])
+    if review.update(review_params)
+      redirect_to "/shelters/#{review.shelter.id}"
+    else
+      flash[:notice] = "Please ensure all required fields are completed"
+      redirect_to "/reviews/#{review.id}/edit"
+    end
   end
 
 
--- spec/features/reviews/edit_spec.rb
+++ spec/features/reviews/edit_spec.rb
@@ -52,6 +52,29 @@ RSpec.describe 'As a visitor' do
         expect(page).to have_current_path "/reviews/#{@review_1.id}/edit"
       end
     end
+
+    describe "On this new page, I see a form that includes that review's pre populated data" do
+      it "the data from creation is prepopulated" do
+        visit("/reviews/#{@review_1.id}/edit")
+
+        expect(find_field('Title').value).to eq(@review_1.title)
+        expect(find_field('Rating').value).to eq(@review_1.rating.to_s)
+        expect(find_field('Content').value).to eq(@review_1.content)
+        # expect(find_field('Optional Picture').value).to eq(@review_1.optional_picture)
+      end
+
+      it "I can update any of these fields and submit the form" do
+        visit("/reviews/#{@review_1.id}/edit")
+
+        fill_in 'Title', with: "If I could give 0 stars I would"
+        fill_in 'Rating', with: 1
+        fill_in 'Content', with: "This place is the worst business in town! My wife Karen and I hate it."
+        # fill_in 'Optional Picture', with: ""
+
+        click_button 'Edit Review'
+        expect(current_path).to eq("/shelters/#{@review_1.shelter.id}")
+      end
+    end
   end
 end
 
