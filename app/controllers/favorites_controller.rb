c7814821c86ea206dc7a53869abab540371b7fb7
Chris Allbritton <cla.inbox@gmail.com>
2020-08-29 10:04:13 -0400

    Update favorites_controller for POROS refactor

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,9 @@
 class FavoritesController < ApplicationController
   def update
     @pet = Pet.find(params[:pet_id])
+    favorites = Favorite.new(session[:favorites])
+    favorites.add_pet(pet.id)
+    session[:favorites] = favorites.contents
     flash[:notice] = "You have added #{@pet.name} to your favorites"
     redirect_to "/pets/#{@pet.id}"
   end
