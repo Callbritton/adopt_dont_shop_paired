fd7e722919b3c6c77fdec56f3bf2a2ce0f556a1c
Chris Allbritton <cla.inbox@gmail.com>
2020-08-29 10:59:31 -0400

    Refactor variables due to favorites helper method providing scope

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,12 @@
 class FavoritesController < ApplicationController
+
   def update
-    @pet = Pet.find(params[:pet_id])
+    pet = Pet.find(params[:pet_id])
     favorites = Favorite.new(session[:favorites])
-    favorites.add_pet(pet.id)
+    favorites.add_pet(pet)
     session[:favorites] = favorites.contents
-    flash[:notice] = "You have added #{@pet.name} to your favorites"
-    redirect_to "/pets/#{@pet.id}"
+    flash[:notice] = "You have added #{pet.name} to your favorites"
+    redirect_to "/pets/#{pet.id}"
   end
 
 end
