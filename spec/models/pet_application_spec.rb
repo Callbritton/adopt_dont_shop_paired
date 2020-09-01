95154dd5f65db9274189ba1c006374c02dace785
Arique <arique.aguilar@gmail.com>
2020-09-01 16:09:05 -0500

    Clean up on aisle models pet_application_spec

--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -1,21 +1,5 @@
 require 'rails_helper'
 
-# User Story 17, Incomplete application for a Pet
-#
-# As a visitor
-# When I apply for a pet and fail to fill out any of the following:
-# - Name
-# - Address
-# - City
-# - State
-# - Zip
-# - Phone Number
-# - Description of why I'd make a good home for this/these pet(s)
-# And I click on a button to submit my application
-# I'm redirect back to the new application form to complete the necessary fields
-# And I see a flash message indicating that I must complete the form in order to submit the application
-
-
   RSpec.describe PetApplication, type: :model do
     describe "validations" do
       it { should validate_presence_of :name}
