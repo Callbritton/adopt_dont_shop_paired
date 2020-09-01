d75c0a5d7c1966d8a58c7ddf32faf4054e5b243a
Arique <arique.aguilar@gmail.com>
2020-09-01 17:27:35 -0500

    Adds model test to favorites, method #add_pet)

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -8,4 +8,26 @@ RSpec.describe Favorite do
       expect(favorite.total_count).to eq(2)
     end
   end
+
+  describe "#add_pet" do
+    it "can add a pet" do
+    @shelter_1 = Shelter.create!(name: "Willy's Animal Center",
+     address: "2020 Whirlwind Dr",
+     city: "Denver",
+     state: "CO",
+     zip: 80014)
+
+
+    @pet_1 = @shelter_1.pets.create!(image: '',
+     name: 'Waylon',
+     approximate_age: 1,
+     sex: 'male',
+     )
+      favorite = Favorite.new(["#{@pet_1.id}"])
+      favorite.add_pet(@pet_1)
+      expect(favorite.total_count).to eq(2)
+    end
+
+  end
+
 end
