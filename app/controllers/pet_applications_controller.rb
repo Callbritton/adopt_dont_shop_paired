41f5be81dad49502fb04a17cf786f153777c7b70
Chris Allbritton <cla.inbox@gmail.com>
2020-09-01 08:34:33 -0400

    Update create action to call PetApplication class

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -4,7 +4,16 @@ class PetApplicationsController < ApplicationController
   end
 
   def create
-    
+    # pets = Pet.find(params[:favorites])
+    PetApplication.new(pet_applications_params)
+  end
+
+
+
+private
+
+  def pet_applications_params
+    params.permit(:name, :address, :city, :state, :zip, :phone_number, :description, :favorites)
   end
 
 end
