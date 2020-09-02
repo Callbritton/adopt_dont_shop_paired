c48b9621a9d0b9361eb9256ac2bac0182234e5ad
Arique <arique.aguilar@gmail.com>
2020-09-01 21:20:02 -0500

    add relationship tests to models to create a joined model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,7 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
+  has_many :pet_application_pets
+  has_many :pet_applications, through: :pet_application_pets
 
   after_initialize :set_defaults
 
--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -2,4 +2,7 @@ class PetApplication < ApplicationRecord
 
   validates_presence_of :name, :address, :city, :state, :zip, :phone_number, :description
 
+  has_many :pets, through: :pet_application_pets
+  has_many :pet_application_pets
+
 end
