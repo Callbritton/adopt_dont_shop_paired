89280d31c6269806fc740a9ffdbfc773b5a3b10b
Arique <arique.aguilar@gmail.com>
2020-09-01 22:38:03 -0500

    Removed pry

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -5,7 +5,6 @@ class PetApplicationsController < ApplicationController
 
   def create
     pet_app = PetApplication.new(pet_applications_params)
-    require "pry"; binding.pry
     if pet_app.save
       pets = (params[:favorites])
       pets.each do |pet|
